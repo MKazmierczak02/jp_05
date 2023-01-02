7dd96ebdc730a34b79715d18e37d56dbf53e2160
Mkazmierczak02 <mmichal.kkazmierczak@gmail.com>
2023-01-02 17:52:15 +0100

    ball handler

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        PlayerPanel playerPanel = new PlayerPanel();
+        PlayerPanel playerPanel2 = new PlayerPanel();
+        HostPanel hostPanel = new HostPanel();
+    }
+}
